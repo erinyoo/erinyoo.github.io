1fe7857689ca6023e7636abaf198956f2d88c8d3
Erin Yoo <erinswyoo@gmail.com>
2023-07-16 23:50:54 -0700

    Add all new photos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import { StaticImage } from 'gatsby-plugin-image';
 const Header: FC = () => {
     return (
         <div className="title header">
-            Hello world! I'm Erin <StaticImage src={"../../images/abouterin.png"} alt={"About Erin"} placeholder={'none'} height={50} />a frontend (dev)otee turned product design aficionado
+            Hello world! <StaticImage src={"../../images/hand.png"} alt={"About Erin"} placeholder={'none'} height={60} /> I'm Erin, a <StaticImage className="code" src={"../../images/code.png"} alt={"About Erin"} placeholder={'none'} height={35} /> frontend devotee turned <StaticImage src={"../../images/pentool.png"} alt={"leftbracket"} placeholder={'none'} height={55} /> product design aficionado
         </div >
     );
 };
